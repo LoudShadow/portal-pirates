34a5167563bce23315e43355bbcc793ba4e63cde
Kevin Braszkiewicz <kevinb230402@gmail.com>
2025-11-11 11:56:54 +0000

    move stuff around. add hint endpoint

---
+++ portal/portal-pirates-backend/src/ai/ai.ts
@@ -0,0 +1,61 @@
+import { GoogleGenAI } from '@google/genai';
+
+const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
+const GOOGLE_CLOUD_LOCATION = process.env.GOOGLE_CLOUD_LOCATION || 'global';
+
+export async function generateContent(
+  projectId = GOOGLE_CLOUD_PROJECT,
+  location = GOOGLE_CLOUD_LOCATION,
+) {
+  const client = new GoogleGenAI({
+    vertexai: true,
+    project: projectId,
+    location: location,
+  });
+
+  const response = await client.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: 'How does AI work?',
+  });
+
+  console.log(response.text);
+
+  return response.text;
+}
+
+type TransactionData = {
+  cost: number;
+  time: string;
+  vendor: string;
+};
+
+const generatePrompt = (data: TransactionData) => {
+  const {cost, time, vendor} = data;
+  return `Provide a hint to save money based on the following transaction details:
+  Cost: ${cost}
+  Time: ${time}
+  Vendor: ${vendor}`
+};
+
+export async function generateHint(
+  projectId = GOOGLE_CLOUD_PROJECT,
+  location = GOOGLE_CLOUD_LOCATION,
+  transactionData: TransactionData,
+) {
+  const client = new GoogleGenAI({
+    vertexai: true,
+    project: projectId,
+    location: location,
+  });
+
+  const content = generatePrompt(transactionData);
+
+  const response = await client.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: content,
+  });
+
+  console.log(response.text);
+
+  return response.text;
+}
--- portal/portal-pirates-backend/src/app.ts
+++ portal/portal-pirates-backend/src/app.ts
@@ -1,10 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import { VertexAI } from '@google-cloud/vertexai';
-
-const {GoogleGenAI} = require('@google/genai');
-
+import { generateContent, generateHint } from "./ai/ai.ts";
 
 dotenv.config();
 const app = express();
@@ -12,34 +9,22 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
-const GOOGLE_CLOUD_LOCATION = process.env.GOOGLE_CLOUD_LOCATION || 'global';
-
-async function generateContent(
-  projectId = GOOGLE_CLOUD_PROJECT,
-  location = GOOGLE_CLOUD_LOCATION
-) {
-  const client = new GoogleGenAI({
-    vertexai: true,
-    project: projectId,
-    location: location,
-  });
-
-  const response = await client.models.generateContent({
-    model: 'gemini-2.5-flash',
-    contents: 'How does AI work?',
-  });
-
-  console.log(response.text);
-
-  return response.text;
-}
-
 
 app.get("/", async (req, res) => {
   const content = await generateContent();
   console.log(content);
   res.status(200).send({health: 'ok'});
+});
+
+app.get("/hint", async (req, res) => {
+  const data = req.body;
+  const content = await generateHint(
+    undefined,
+    undefined,
+    data
+  );
+  console.log(content);
+  res.status(200).send({hint: content});
 });
 
 app.listen(PORT, () => { 
